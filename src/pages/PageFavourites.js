57bf8e10ad9836db81213311ce47cf95e0e68d59
Jessica <jessicalaporte888@gmail.com>
2022-04-28 17:26:37 -0700

    Design Favourite Page

--- src/pages/PageFavourites.js
+++ src/pages/PageFavourites.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { appTitle } from '../globals/globals';
+import likeIcon from '../media/like-icon.png';
 
 function PageFavourites() {
 
@@ -8,7 +9,10 @@ function PageFavourites() {
     }, []);
 
     return (
-        <h1>Hi</h1>
+        <div className="favourite-content">
+            <h1>You don't have any movies in your favourites.</h1>
+            <p>To add movies to your favourites, click <img src={likeIcon} alt="like icon with a heart shape"/> on the movie cards.</p>
+        </div>
     )
 }
 export default PageFavourites
